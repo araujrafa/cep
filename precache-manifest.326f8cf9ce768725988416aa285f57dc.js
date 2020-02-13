9aa05041dbd6fed25fab336e17508ec396b0f172
Rafael Araujo <araujrafa@MacBook-Air-de-Rafael.local>
2020-02-13 19:04:33 -0300

    Updates

---
+++ precache-manifest.326f8cf9ce768725988416aa285f57dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2349035ccdc1822cb11d24838cdf6d2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1acb745ad91d739cea8",
+    "url": "/static/js/2.6742cb6b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.6742cb6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78e8072ffc42b3d4294d",
+    "url": "/static/js/main.7c2eb63b.chunk.js"
+  },
+  {
+    "revision": "30d538bbadb9431c22a6",
+    "url": "/static/js/runtime-main.1bfe09a6.js"
+  }
+]);
